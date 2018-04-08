ee69616b2a52872cb896fbf9716d8c38e37e1ad5
unknown <williamwlxu@gmail.com>
2018-04-07 23:10:01 -0700

    Initial commit

---
+++ src/main/navigation/Selection.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import "./Selection.css";
+
+// map out genres, pass down on genrechange to dropdown list
+const Selection = ({ genres, genre, onGenreChange }) => (
+  <div className="selection">
+    <label>Genre</label>
+    <select value={genre} onChange={onGenreChange}>
+      {genres.map( genre => (
+        <option key={genre.id} value={genre.name}>{genre.name}</option>
+      ))}
+    </select>
+  </div>
+)
+
+export default Selection;
